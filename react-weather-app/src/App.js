ab3d3def5d36f29f02dfe88d5c728a25644e3171
etay1 <etay1@brockport.edu>
2023-12-13 18:26:18 -0500

    [FIX] submit button

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,51 +1,53 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [days, setDays] = useState(1);
   const [temperatures, setTemperatures] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(`https://api.weather.gov/points/${city}`);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.weather.gov/points/${city}`
+      );
 
-        // Extract the forecast URL from the response
-        const forecastURL = response.data.properties.forecast;
+      // Extract the forecast URL from the response
+      const forecastURL = response.data.properties.forecast;
 
-        // Fetch the forecast data using the obtained URL
-        const forecastResponse = await axios.get(forecastURL);
+      // Fetch the forecast data using the obtained URL
+      const forecastResponse = await axios.get(forecastURL);
 
-        // Extract temperatures from the forecast response based on selected days
-        const temps = forecastResponse.data.properties.periods
-          .slice(0, days)
-          .map((period) => period.temperature);
+      // Extract temperatures from the forecast response based on selected days
+      const temps = forecastResponse.data.properties.periods
+        .slice(0, days)
+        .map((period) => period.temperature);
 
-        setTemperatures(temps);
-      } catch (error) {
-        console.error('Error fetching data:', error.response || error.message || error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (city && days) {
-      fetchData();
+      setTemperatures(temps);
+    } catch (error) {
+      console.error(
+        "Error fetching data:",
+        error.response || error.message || error
+      );
+    } finally {
+      setLoading(false);
     }
-  }, [city, days]);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (city && days) {
+      fetchData();
+    }
   };
 
   return (
-    <div>
-      <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="page">
+      <h1 className="title">Weather App</h1>
+      <form className="form" onSubmit={handleSubmit}>
         <label>
           Select City:
           <select value={city} onChange={(e) => setCity(e.target.value)}>
@@ -69,7 +71,7 @@ function App() {
             <option value="7">7 days</option>
           </select>
         </label>
-        <button type="submit">Submit</button>
+        <button className="button" type="submit">Submit</button>
       </form>
 
       {loading ? (
@@ -80,7 +82,7 @@ function App() {
             <p>{`Temperature in ${city} for the next ${days} day(s):`}</p>
             <ul>
               {temperatures.map((temp, index) => (
-                <li key={index}>{`${index + 1} day: ${temp}°F`}</li>
+                <li key={index}>{`Day ${index + 1}: ${temp}°F`}</li>
               ))}
             </ul>
           </div>
