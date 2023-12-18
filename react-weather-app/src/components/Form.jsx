46bc3aab6c0d99b4b6d2807d1fa8b853eb2e43a7
etay1 <etay1@brockport.edu>
2023-12-18 00:29:55 -0500

    [ADD] error if no city is selected

--- react-weather-app/src/components/Form.jsx
+++ react-weather-app/src/components/Form.jsx
@@ -1,32 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = ({ city, setCity, days, setDays, handleSubmit, cityCoordinates }) => (
-  <form className="form" onSubmit={handleSubmit}>
-    <label>
-      Select City:
-      <select value={city} onChange={(e) => setCity(e.target.value)}>
-        <option value="">Select a city</option>
-        {Object.keys(cityCoordinates).map((cityName) => (
-          <option key={cityName} value={cityName}>
-            {cityName}
-          </option>
-        ))}
-      </select>
-    </label>
-    <label>
-      Select Days:
-      <select value={days} onChange={(e) => setDays(e.target.value)}>
-        {[1, 2, 3, 4, 5, 6, 7].map((num) => (
-          <option key={num} value={num}>
-            {`${num} day(s)`}
-          </option>
-        ))}
-      </select>
-    </label>
-    <button className="button" type="submit">
-      Submit
-    </button>
-  </form>
-);
+const Form = ({
+  city,
+  setCity,
+  days,
+  setDays,
+  handleSubmit,
+  cityCoordinates,
+}) => {
+  const [error, setError] = useState(null);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault(); // Prevent default form submission behavior.
+
+    if (!city) {
+      setError("Please select a city before submitting.");
+      return;
+    }
+
+    setError(null);
+
+    try {
+      await handleSubmit(e); // Pass the event object to handleSubmit.
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  return (
+    <form className="form" onSubmit={handleFormSubmit}>
+      <label>
+        Select City:
+        <select value={city} onChange={(e) => setCity(e.target.value)}>
+          <option value="">Select a city</option>
+          {Object.keys(cityCoordinates).map((cityName) => (
+            <option key={cityName} value={cityName}>
+              {cityName}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label>
+        Select Days:
+        <select value={days} onChange={(e) => setDays(e.target.value)}>
+          {[1, 2, 3, 4, 5, 6, 7].map((num) => (
+            <option key={num} value={num}>
+              {`${num} day(s)`}
+            </option>
+          ))}
+        </select>
+      </label>
+      <button className="button" type="submit">
+        Submit
+      </button>
+      {error && <div className="error-message">{error}</div>}
+    </form>
+  );
+};
 
 export default Form;
